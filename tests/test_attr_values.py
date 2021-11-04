79191cc4f01d3209203eb7bc618c8eb1582f4dd2
Roberta Takenaka <roberta.takenaka@scielo.org>
2021-11-03 21:10:48 -0300

    Cria classe e funções para lidar com conversão de valores de atributos encontrado em ISIS e em SPS

---
+++ tests/test_attr_values.py
@@ -0,0 +1,43 @@
+from unittest import TestCase
+
+
+from scielo_migration.attr_values import (
+    AttrValues,
+)
+
+
+class TestAttrValues(TestCase):
+
+    def test_isis_value(self):
+        data = [
+            {"isis": "oa", "sps": "research-article"},
+        ]
+        attr_values = AttrValues(data)
+        expected = "oa"
+        result = attr_values.get_isis_value("research-article")
+        self.assertEqual(expected, result)
+
+    def test_sps_value(self):
+        data = [
+            {"isis": "oa", "sps": "research-article"},
+        ]
+        attr_values = AttrValues(data)
+        expected = "research-article"
+        result = attr_values.get_sps_value("oa")
+        self.assertEqual(expected, result)
+
+    def test_isis_value_returns_none(self):
+        data = [
+            {"isis": "oa", "sps": "research-article"},
+        ]
+        attr_values = AttrValues(data)
+        result = attr_values.get_isis_value("abstract")
+        self.assertIsNone(result)
+
+    def test_sps_value_returns_none(self):
+        data = [
+            {"isis": "oa", "sps": "research-article"},
+        ]
+        attr_values = AttrValues(data)
+        result = attr_values.get_sps_value("xyz")
+        self.assertIsNone(result)
