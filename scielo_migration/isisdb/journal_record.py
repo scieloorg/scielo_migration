82937c00a746afc8aa257c7f5fdada7cdca35402
Roberta Takenaka <roberta.takenaka@scielo.org>
2021-11-09 08:44:37 -0300

    Cria os atributos print_issn e electronic_issn

--- scielo_migration/isisdb/journal_record.py
+++ scielo_migration/isisdb/journal_record.py
@@ -10,3 +10,23 @@ class JournalRecord(BaseJournalRecord):
             data_dictionary=None):
         super().__init__(
             record, multi_val_tags, data_dictionary)
+
+    @property
+    def print_issn(self):
+        if not hasattr(self, '_print_issn'):
+            self._print_issn = None
+            for issn in self.issns:
+                if issn['type'] == 'PRINT':
+                    self._print_issn = issn["value"]
+                    break
+        return self._print_issn
+
+    @property
+    def electronic_issn(self):
+        if not hasattr(self, '_electronic_issn'):
+            self._electronic_issn = None
+            for issn in self.issns:
+                if issn['type'] == 'ONLIN':
+                    self._electronic_issn = issn["value"]
+                    break
+        return self._electronic_issn
