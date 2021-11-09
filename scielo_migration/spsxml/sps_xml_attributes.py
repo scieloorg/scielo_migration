5a0cba7434a3c2522989a8297b972d0e0f7e95b9
Roberta Takenaka <roberta.takenaka@scielo.org>
2021-11-09 09:31:17 -0300

    Inclui dados de países e disponibiliza o acesso pela classe Country

--- scielo_migration/spsxml/sps_xml_attributes.py
+++ scielo_migration/spsxml/sps_xml_attributes.py
@@ -25,4 +25,32 @@ def _load_values(filename):
     return _get_dict(_read_csv_file(file_path))
 
 
+def _load_countries(filename):
+    file_path = os.path.join(ATTRIBUTES_PATH, filename)
+    data = {}
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data[row['alpha_2_code']] = row
+    return data
+
+
+class Country:
+
+    def __init__(self, items):
+        self._items = items
+
+    def name(self, code):
+        try:
+            country = self._items[code]
+        except KeyError:
+            return
+
+        for k in ("short_name_en", "short_name_pt", "short_name_es"):
+            name = country.get(k)
+            if name:
+                return name
+
+
 ARTICLE_TYPES = _load_values('isis2sps_article_types.csv')
+COUNTRY_ITEMS = Country(_load_countries("country.csv"))
