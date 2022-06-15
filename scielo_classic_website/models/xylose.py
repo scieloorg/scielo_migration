752e69118bb72220abc3db5adfae47184cda0b8d
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-06-15 18:38:34 -0300

    Adiciona a classe que deve ser usada para prover uma assinatura como do AM

---
+++ scielo_classic_website/models/xylose.py
@@ -0,0 +1,29 @@
+from scielo_migration.isisdb import models
+
+
+class Journal(models.Journal):
+    def __init__(self, data):
+        super().__init__(data)
+
+
+class Article(models.Document):
+
+    def __init__(self, data):
+        super().__init__(data)
+
+    @property
+    def start_page(self):
+        return self.page.get("start")
+
+    @property
+    def end_page(self):
+        return self.page.get("end")
+
+    @property
+    def start_page_sequence(self):
+        return self.page.get("sequence")
+
+    @property
+    def elocation(self):
+        print("page", self.page)
+        return self.page.get("elocation")
