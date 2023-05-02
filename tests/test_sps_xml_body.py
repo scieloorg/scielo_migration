7bc56ef23adf20d77fcfca71fe61e19eb1026426
rg3915 <regis42santos@gmail.com>
2023-05-02 05:59:35 -0300

    test: Adiciona teste TestTagsHPipe. close #14

--- tests/test_sps_xml_body.py
+++ tests/test_sps_xml_body.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 from lxml import etree
 
-from scielo_classic_website.spsxml.sps_xml_body_pipes import OlPipe, UlPipe
+from scielo_classic_website.spsxml.sps_xml_body_pipes import OlPipe, TagsHPipe, UlPipe
 
 
 def get_tree(xml_str):
@@ -43,7 +43,36 @@ class TestUlPipe(TestCase):
             "</root>"
         )
         data = (raw, xml)
-        _raw, _xml = UlPipe().transform(data)
+
+        _, _xml = UlPipe().transform(data)
         result = etree.tostring(_xml, encoding="utf-8").decode("utf-8")
+
         self.assertEqual(1, len(_xml.xpath(".//list[@list-type='bullet']")))
+        self.assertEqual(expected, result)
+
+
+class TestTagsHPipe(TestCase):
+    def test_transform_substitui_tags_de_cabecalho_por_tags_title(self):
+        xml = get_tree(
+            "<root><body><h1>Título 1</h1><h2>Título 2</h2><h3>Título 3</h3></body></root>"
+        )
+        content = (
+            "<root>"
+            "<body>"
+            '<title content-type="h1">Título 1</title>'
+            '<title content-type="h2">Título 2</title>'
+            '<title content-type="h3">Título 3</title>'
+            "</body>"
+            "</root>"
+        )
+
+        # Cria objeto etree a partir de content.
+        expected_element = etree.fromstring(content)
+        data = (None, xml)
+
+        _, transformed_xml = TagsHPipe().transform(data)
+
+        expected = etree.tostring(expected_element, encoding="utf-8").decode("utf-8")
+        result = etree.tostring(transformed_xml, encoding="utf-8").decode("utf-8")
+
         self.assertEqual(expected, result)
