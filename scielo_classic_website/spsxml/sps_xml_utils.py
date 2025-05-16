8b3810975a935814114fb50779f3989b09a66bcb
Roberta Takenaka <roberta.takenaka@scielo.org>
2025-05-16 18:28:40 -0300

    Cria set_subject_text()

---
+++ scielo_classic_website/spsxml/sps_xml_utils.py
@@ -0,0 +1,15 @@
+from lxml import etree as ET
+
+from scielo_classic_website.exceptions import GetSectionTitleException
+
+
+def set_subject_text(subject_node, document, language):
+    try:
+        subject_node.text = document.get_section_title(language)
+    except GetSectionTitleException as e:
+        comment = ET.Comment(str(e))
+        subject_node.append(comment)
+        try:
+            subject_node.text = document.get_sections()[0]["text"]
+        except (IndexError, KeyError):
+            pass
